a9f343f19c947537035a46f92171fd14e8902bdc
Matthew Puccerella <matthewpucc@gmail.com>
2011-06-24 18:20:53 -0500

    Created Random BG Generator
    This will draw a random pattern on a canvas

---
+++ random-background/canvas-random-generator.js
@@ -0,0 +1,68 @@
+(function(undefined) {
+	
+	var settings = {
+		DEBUG: true,
+		baseColor: false,
+		count: 250,
+		colorMaxVariance: 25,
+		lineLength: 2
+	};
+	
+	window.randomizeCanvas = function( canvas_id,  options ) {
+		
+		var utilities = { 
+			ohNoMrBill : function( error_msg, DEBUG ) {
+				if( window.console != undefined && cs.DEBUG ) {
+					window.console.error( error_msg );
+				}
+				return;
+			},
+			drawLine: function( start, finish, color ) {
+				context.beginPath();
+				context.strokeStyle = "rgb( " + color[0] + ", " + color[1] + ", " + color[2] + ")";
+				context.moveTo( start.x, start.y );
+				context.lineTo( finish.x, finish.y );
+				context.stroke();
+				context.closePath();
+			}
+		};
+		
+		var cs = Object.merge( settings, options );
+		
+		var canvas = document.id( canvas_id );
+		
+		if( !canvas ) {
+			return utilities.ohNoMrBill( 'Canvas could not be found' );
+		}
+		
+		var context = canvas.getContext( '2d' );
+		
+		var canvasWidth = canvas.getAttribute('width');
+		var canvasHeight = canvas.getAttribute('height');
+		
+		var thiscolor;
+		var start = {};
+		var finish = {};
+		
+		for( var i = 0; i < cs.count; i++ ) {
+			
+			if( cs.baseColor != false ) {
+				thisColor = Object.clone( cs.baseColor );
+				thisColor = thisColor.setBrightness( thisColor.hsb[2] + $random( cs.colorMaxVariance * -1, cs.colorMaxVariance ) );
+			} else {
+				thisColor = $RGB( $random( 0, 255 ), $random( 0, 255 ), $random( 0, 255 ) );
+			} 
+			start = {
+				x: $random( 0, canvasWidth ),
+				y: $random( 0, canvasHeight )
+			};
+			end = {
+				x: start.x + $random( -1 * cs.lineLength, cs.lineLength ),
+				y: start.y + $random( -1 * cs.lineLength, cs.lineLength ),
+			};
+			
+			utilities.drawLine( start, end, thisColor );
+		}
+	}
+}
+)();
